5e10efc75416172af3ae1f73bc2d99b3a9ecc7c9
hanlei <13609850398@139.com>
2021-09-03 14:15:06 +0800

    引入vxe-table，调整打包脚本

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -5,9 +5,13 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
+const sassOptions = {
+  includePaths: '../../node_modules/'
+};
+
 function compile() {
   return src('./src/*.scss')
-    .pipe(sass.sync())
+    .pipe(sass.sync(sassOptions))
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
