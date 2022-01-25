6a29c731987d2cf1a60c0febb19e51ce7ce18f8c
tenbamboo <liu19891014@163.com>
2022-01-25 16:42:44 +0800

    fixbug:labelMode状态下，也一并隐藏了自定义按钮的问题

--- packages/pro-table/src/table-mixin.js
+++ packages/pro-table/src/table-mixin.js
@@ -641,7 +641,7 @@ export default {
         ROW_MANUAL_SAVE_BUTTON
       } = this;
       // label模式时，直接隐藏操作列
-      if (!this.showControlColumn || labelMode) {
+      if (!this.showControlColumn) {
         return;
       }
       return {
