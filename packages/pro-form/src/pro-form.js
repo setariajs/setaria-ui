21d04f95248b9338cd67a83302985c6f03b1758d
hanlei <hanlei@cn.ibm.com>
2022-03-16 16:50:17 +0800

    [ProForm]修复抛出异常时，使用finally导致异常被抛出两次的问题

--- packages/pro-form/src/pro-form.js
+++ packages/pro-form/src/pro-form.js
@@ -242,8 +242,8 @@ export default {
             if (result.then) {
               result.then(callbackFunc);
             }
-            if (result.finally) {
-              result.finally(callbackFunc);
+            if (result.catch) {
+              result.catch(callbackFunc);
             }
           }
           this.$emit('submit');
