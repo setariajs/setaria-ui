c61f50aa7c0824df2ee59dafbb050ad561c6486f
tenbamboo <liu19891014@163.com>
2023-04-18 19:25:57 +0800

    [Edittable]修复问题

--- packages/pro-table/src/editable-pro-table.js
+++ packages/pro-table/src/editable-pro-table.js
@@ -82,9 +82,9 @@ export default Vue.extend({
         showPagination
       } = this;
       // 数据变更时需要清空选择的数据
-      if (!this.isReserve) {
-        this.emitSelectionChange([]);
-      }
+      // if (!this.isReserve) {
+      //   this.emitSelectionChange([]);
+      // }
       if (_.isEmpty(data)) {
         // eslint-disable-next-line vue/no-side-effects-in-computed-properties
         this.innerTotal = 0;
